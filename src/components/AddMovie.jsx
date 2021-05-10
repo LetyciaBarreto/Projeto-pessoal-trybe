e5ce24b6f5a063f64e865c27e4e6490ef2867b28
Letycia Barreto <letyciasbarreto@gmail.com>
2021-05-09 22:43:59 -0300

    R 9, 10, 11 teste para depois redistribuir em componentes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,26 +4,25 @@ import React from 'react';
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
-    this.handleOnChange = this.handleOnChange.bind(this);
 
     this.state = {
-    //   subtitle: '',
+      subtitle: '',
       title: '',
-    //   imagePath: '',
-    //   storyline: '',
+      imagePath: '',
+      storyline: '',
     //   rating: 0,
     //   genre: 'action',
     };
   }
 
-  handleOnChange({ target: { value } }) {
+  handleOnChange = ({ target: { value, name } }) => {
     this.setState({
-      title: value,
+      [name]: value,
     });
   }
 
   render() {
-    const { title } = this.state;
+    const { title, subtitle, imagePath, storyline } = this.state;
     // const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
@@ -33,9 +32,46 @@ class AddMovie extends React.Component {
         >
           Título
           <input
-            id="title"
+            name="title"
             data-testid="title-input"
             value={ title }
+            onChange={ this.handleOnChange }
+          />
+        </label>
+        <label
+          htmlFor="subtitle"
+          data-testid="subtitle-input-label"
+        >
+          Subtítulo
+          <input
+            name="subtitle"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            onChange={ this.handleOnChange }
+          />
+        </label>
+        <label
+          htmlFor="imagePath"
+          data-testid="image-input-label"
+        >
+          Imagem
+          <input
+            type="text"
+            name="imagePath"
+            data-testid="image-input"
+            value={ imagePath }
+            onChange={ this.handleOnChange }
+          />
+        </label>
+        <label
+          htmlFor="storyline"
+          data-testid="storyline-input-label"
+        >
+          Sinopse
+          <textarea
+            name="storyline"
+            data-testid="storyline-input"
+            value={ storyline }
             onChange={ this.handleOnChange }
           />
         </label>
