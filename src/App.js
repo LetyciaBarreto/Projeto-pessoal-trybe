a27b322b8669b3ba1ad961d48749ba4b50539c42
Letycia Barreto <letyciasbarreto@gmail.com>
2021-05-16 14:47:47 -0300

    R1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-    <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact patch="/" component={ MovieList } />
+        <Route exact patch="/movies/:id" component={ MovieDetails } />
+        <Route exact patch="/movies/new" component={ NewMovie } />
+        <Route exact patch="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
